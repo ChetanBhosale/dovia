73fda9631cca429a1d0789caf5103f2a11945fae
Chetan <chetan@Chetans-MacBook-Air.local>
2025-08-07 21:55:14 +0530

    dockerFile and cloud processing

--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,5 +1,8 @@
 import { inngest } from "./client";
 import { createAgent, gemini } from "@inngest/agent-kit";
+import {Sandbox} from '@e2b/code-interpreter'
+import { getSandbox } from "./utils";
+
 export const helloWorld = inngest.createFunction(
   {
     id: "hello-world",
@@ -8,7 +11,11 @@ export const helloWorld = inngest.createFunction(
     event: "test/hello-world",
   },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "5s");
+    // await step.sleep("wait-a-moment", "5s");
+    const sandboxId = await step.run("run-sandbox", async () => {
+        const sandbox = await Sandbox.create('dovia')
+        return sandbox.sandboxId
+    });
 
     const agentBrain = createAgent({
       name: "agent-brain",
@@ -18,7 +25,11 @@ export const helloWorld = inngest.createFunction(
 
     const { output } = await agentBrain.run(`BUILD : ${event.data.value}`);
 
-    console.log(output);
+    const sandboxUrl =  await step.run("wait-for-sandbox", async () => {
+      const sandbox = await getSandbox(sandboxId)
+      const host =  sandbox.getHost(3000)
+      return `https://${host}`
+    })
 
     return { message: output };
   }
